7e22c984ba3756e3329ecfa46b67c299127120bc
Arva Kagdi <60667228+arvakagdi@users.noreply.github.com>
2021-01-11 10:00:38 -0500

    javascript expressions in JSX

--- src/index.js
+++ src/index.js
@@ -23,3 +23,22 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+const d = new Date();
+const currYear = d.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p> Created by {fName + " " + lName}.</p>
+    <p> Copyright {currYear}.</p>
+  </div>,
+  document.getElementById("root")
+);
