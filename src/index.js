54048fdb103603291d3ac713c98db0c1398a4826
Arva Kagdi <60667228+arvakagdi@users.noreply.github.com>
2021-01-11 10:44:33 -0500

    Styling attributes

--- src/index.js
+++ src/index.js
@@ -9,20 +9,20 @@ const fName = "Arva";
 const lName = "Kagdi";
 const luckyNumber = 7;
 
-ReactDOM.render(
-  <div>
-    {/* <h1>To do list for {fName} {lName} </h1> */}
-    <h1> Hello {`${fName} ${lName}`}</h1>{" "}
-    {/* Using string literals for injecting js. Also known as string interpolation */}
-    <ul>
-      <li> code </li>
-      <li> apply </li>
-      <li> study </li>
-    </ul>
-    <p> My luckyNumber is {luckyNumber}.</p>
-  </div>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <div>
+//     {/* <h1>To do list for {fName} {lName} </h1> */}
+//     <h1 className='heading'> Hello {`${fName} ${lName}`}</h1>{" "}
+//     {/* Using string literals for injecting js. Also known as string interpolation */}
+//     <ul>
+//       <li> code </li>
+//       <li> apply </li>
+//       <li> study </li>
+//     </ul>
+//     <p> My luckyNumber is {luckyNumber}.</p>
+//   </div>,
+//   document.getElementById("root")
+// );
 
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
@@ -32,13 +32,23 @@ ReactDOM.render(
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
-const d = new Date();
-const currYear = d.getFullYear();
+// const d = new Date();
+// const currYear = d.getFullYear();
 
+// ReactDOM.render(
+//   <div>
+//     <h1 className="heading" contentEditable = "True" spellCheck = "false"> Created by {fName + " " + lName}.</h1>
+//     <p> Copyright {currYear}.</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+const randomImage = "https://picsum.photos/200";
 ReactDOM.render(
   <div>
-    <p> Created by {fName + " " + lName}.</p>
-    <p> Copyright {currYear}.</p>
+    <h1 className="heading">{fName + " " + lName + "'s"} Random clicks!</h1>
+    <div>
+      <img className="food-img" alt="random" src={randomImage} />
+    </div>
   </div>,
   document.getElementById("root")
 );
