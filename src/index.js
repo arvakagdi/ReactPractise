a5967ebc46943092ee27a7138d98b985f6f1243e
Arva Kagdi <60667228+arvakagdi@users.noreply.github.com>
2021-01-11 09:24:12 -0500

    JavaScript expressions inserting JS in HTML code inside JS

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Arva";
+const luckyNumber = 7;
+
 ReactDOM.render(
   <div>
-    <h1>To do list</h1>
+    <h1>To do list for {name} </h1>
     <ul>
       <li> code </li>
       <li> apply </li>
       <li> study </li>
     </ul>
+    <p> My luckyNumber is {luckyNumber}.</p>
   </div>,
   document.getElementById("root")
 );
