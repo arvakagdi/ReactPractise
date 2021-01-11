2fd05a7421b89d4b65ef29039d5f80a8b4444d59
Arva Kagdi <60667228+arvakagdi@users.noreply.github.com>
2021-01-11 09:32:20 -0500

    using string literals

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Arva";
+const fName = "Arva";
+const lName = "Kagdi";
 const luckyNumber = 7;
 
 ReactDOM.render(
   <div>
-    <h1>To do list for {name} </h1>
+    {/* <h1>To do list for {fName} {lName} </h1> */}
+    <h1> Hello {`${fName} ${lName}`}</h1>{" "}
+    {/* Using string literals for injecting js. Also known as string interpolation */}
     <ul>
       <li> code </li>
       <li> apply </li>
